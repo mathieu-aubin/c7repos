febed51795d1d3b937e81d03b92f10b299024d89
old-fart <mathieu@zeroserieux.com>
2017-09-22 09:25:06 -0400

    Minor syntax changes/corrections

--- c7repos.sh
+++ c7repos.sh
@@ -416,12 +416,12 @@ _installCOMMON() {
 
 # Function to create our dotfiles
 _createDOTFILES() {
-    # Appending some stuff to .bashrc
+    # Append some stuff to .bashrc
     echo -e "\033[1mAppending to ${HOME}/.bashrc...\033[0m" ; sleep 0.2 ;
     #echo -e "\nshopt -s checkwinsize\nshopt -s histappend\nexport HISTIGNORE='&:exit:x:l:history:editenv:[h ]*:[ \\\t]*:?:??:w -i:pwd:srcalias*:srcexports*:srcfunctions*:srccolors*:srcdot*:env:quit:popd*:ginx*:chownwww*:yum clean*:yum makecache*'\nexport HISTCONTROL=ignoreboth\nexport HISTSIZE=20000\nexport HISTFILESIZE=100000\nexport HISTTIMEFORMAT=\"%Y/%m/%d %T \"" >> ${HOME}/.bashrc ;
     echo -e "\nshopt -s checkwinsize\nshopt -s histappend\nexport HISTCONTROL=ignoreboth\nexport HISTSIZE=20000\nexport HISTFILESIZE=100000\nexport HISTTIMEFORMAT=\"%Y/%m/%d %T \"" >> ${HOME}/.bashrc ;
     echo -e "\n# Source ${HOME}/.bash_* files not excluded by the grep command" >> ${HOME}/.bashrc ;
-    echo 'for DFs in $(ls -1p $HOME/.bash_* | grep -E -v "history|logout|profile|back|/$"); do source $DFs; done' >> ${HOME}/.bashrc ;
+    echo 'for DFs in $(ls -1p ${HOME}/.bash_* | grep -E -v "history|logout|profile|back|/$"); do source ${DFs}; done' >> ${HOME}/.bashrc ;
 
     # Creates the functions file
     echo -e "\033[1mCreating functions file at ${HOME}/.bash_functions...\033[0m" ; sleep 0.2 ;
